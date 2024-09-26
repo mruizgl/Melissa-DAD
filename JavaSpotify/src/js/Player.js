9ce3121b2b17d6fdaa2cc64b2e0d24afa5d9ba0b
Melissa Ruiz <melruizglez97@gmail.com>
2024-09-26 20:28:22 +0100

    [JavaSpotify]: se avanza en proyecto

---
+++ JavaSpotify/src/js/Player.js
@@ -0,0 +1,17 @@
+import Song, { play_song } from './Song';
+
+export default class Player {
+    constructor(map) {
+        Object.entries(map);
+        let aux = 1;
+        for (var[key, value] of Object.entries(map)) {
+
+            aux++;
+
+        }
+
+
+    }
+    
+    
+}
---
+++ JavaSpotify/src/js/index.js
@@ -0,0 +1,12 @@
+import songs from '../assets/songs/*.mp3';
+
+Object.keys(songs);
+
+const map = {};
+
+let aux = 1;
+for (var key of Object.keys(songs)) {
+    map[`item-${aux}`] = songs[key];
+aux++;
+
+}
